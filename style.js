fea1282bf8495e3c24eed7d2bbc3fdd3929d64ba
Rodriguez-Sanchez <cedric38170@hotmail.com>
2024-01-30 11:42:57 +0100

    Déclaration des éléments à récupéré + activation des flèche et des bullets point

--- style.js
+++ style.js
@@ -9,6 +9,7 @@ const bannerText = document.querySelector("#banner p");
 //Bullet point
 const dotsContainer = document.getElementById("dots-Container");
 const dots = document.querySelectorAll(".dot");
+let index = 0;
 // Déclaration d'un tableau pour gérer les différentes images et texte associé
 const slides = [
 	{
@@ -26,5 +27,27 @@ const slides = [
 	{
 		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+	},
+];
+
+// Activation de la flèche de gauche lors du click
+
+	arrowLeft.addEventListener("click", () => {
+		index--;
+		if(0 > index) index = 3;
+		console.log(index);
+		bannerImage.src = "images/slideshow/"+ slides[index].image;
+		bannerText.innerHTML = slides[index].tagLine;
+		//console.log(clickLeft)
+	});
+
+	arrowRight.addEventListener("click", () => {
+		dots[index].className = "dot";
+		index++;
+		if(4 == index) index = 0;
+		console.log(index);
+		bannerImage.src = "images/slideshow/"+ slides[index].image;
+		bannerText.innerHTML = slides[index].tagLine;
+		//console.log(clickLeft)
+		dots[index].className = "dot dot_selected";
+	});
